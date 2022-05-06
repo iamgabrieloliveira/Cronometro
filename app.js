d42bcb35e3e2aa2e368af6acaa7dd4897c461190
iamgabrieloliveira <oliveiragabriel.dev@gmail.com>
2022-05-06 16:32:21 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,50 @@
+var intervalo;
+var milisec = 0;
+var sec = 0;
+var min = 0;
+var hour = 0;
+
+function milisegundos(digit){
+    if(digit < 100 & digit < 1000){
+        return '0' + digit
+    }else if (digit < 10){
+        return '0' + digit
+    }else 
+    return digit
+    }
+function twoDigits(digit){
+    if(digit < 10){
+        return '0' + digit
+    }else 
+    return digit
+}
+
+function start(){
+    intervalo = setInterval(watch, 10)
+}
+
+function pause(){
+    clearInterval(intervalo)
+}
+
+function stop(){
+    clearInterval(intervalo)
+    document.querySelector('.watch').innerHTML = '00:00:00:000'
+}
+
+function watch(){
+    
+    if((milisec += 10) == 1000){
+        milisec = 00;
+        sec++
+    }
+    if(sec == 60){
+        sec = 0;
+        min ++
+    }
+    if(min == 60){
+        min = 0;
+        hour ++
+    }
+    document.querySelector('.watch').innerHTML = twoDigits(hour) + ':' + twoDigits(min) + ':' + twoDigits(sec)  + ':' + milisegundos(milisec)
+}
